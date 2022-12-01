45a3b4a8dfdc3b9b926700b1292e3de6fe2c4e9e
jenmwa <jenmwa@gmail.com>
2022-12-01 06:52:04 +0100

    saving

--- script.js
+++ script.js
@@ -1,49 +1,41 @@
 document.querySelector('#inputs').innerHTML = `
 <section class="formOrder" id="formOrder">
-  <form id="formContainer">
-
+  <form>
     <label class="side">
     Namn:* 
       <input type="text" id="firstname" name="firstname" placeholder="Förnamn" /><br>
       <div id="nameError"></div>
     </label>
-
     <label class="side">
     Efternamn:* 
       <input type="text" id="lastname" name="lastname" placeholder="Efternamn"/> <br>
       <div id="lastnameError"></div>
     </label>
-
     <label class="side">
     Adress (gata):* 
       <input type="text" id="adress" name="adress" placeholder="Adress (gata)"/> <br>
       <div id="adressError"></div>
     </label>
-
     <label class="side">
     ev Portkod: 
       <input type="text" id="portkod" name="portkod" placeholder="portkod "/> <br>
     </label>
-
     <label class="side">
     Postnummer:* 
       <input type="text" id="zip" name="zip" placeholder="postnummer" /><br>
       <div id="zipError"></div>
     </label>
-
     <label class="side">
     Postort:* 
       <input type="text" id="city" name="city" placeholder="postort" /><br>
       <div id="cityError"></div>
     </label>
-
     <label>
     <label class="side">
       Telefonnummer (mobil):     
       (+46)<input type="tel" id="phoneNumber" name="phoneNumber" placeholder="07xxxxxxxx"/> <br>
       <div id="phoneError"></div>
     </label>
-
     <label class="side">
     Epost:* 
       <input type="text" id="email" name="email" placeholder="E-postadress"/> <br>
@@ -56,29 +48,24 @@ document.querySelector('#inputs').innerHTML = `
           <input type="radio" name="paymentOptions" id="debitCredit" />
           <span>Debit/Kreditkort</span>
         </label>
-
         <label>
           <input type="radio" name="paymentOptions" id="invoice" />
           <span>Faktura</span>
         </label>
-
         <div class="cardPayment">
           <label class="side">
             <span>Kortnummer:</span><br />
             <input type="number" name="cardNumber" id="cardNumber" /><br />
           </label>
-
           <label class="side">
             <span>Datum/år:</span><br />
             <input type="month" name="cardDate" id="cardDate" /><br />
           </label>
-
           <label class="side">
             <span>CVC:</span><br />
             <input type="number" name="cardSecurity" id="cardSecurity" />
           </label>
         </div>
-
         <div class="invoicePayment">
             <label class="side">
             <span>Personnummer:*</span><br>
@@ -92,12 +79,10 @@ document.querySelector('#inputs').innerHTML = `
           </label>
         </div>
       </fieldset>
-
       <label>
         <input type="checkbox" id="gdpr" name="gdpr" required />
         <span>Godkännande för behandling av personuppgifter.*</span><br />
       </label>
-
       <label>
         <input type="checkbox" id="newsletter" name="newsletter" checked />
         <span>Få vårat nyhetsbrev en gång i veckan för alla nyheter och erbjudanden.</span><br />
@@ -111,7 +96,6 @@ document.querySelector('#inputs').innerHTML = `
 /************* VARIABLES INPUTS *************************************************/
 
 const submitBtn = document.querySelector('#submit');
-//const resetBtn = document.querySelector('#reset');
 
 const formOrderFirstName = document.querySelector('#firstname');
 const formOrderLastName = document.querySelector('#lastname');
@@ -127,7 +111,7 @@ const invoiceRadio = document.querySelector('#invoice');
 const cardPayment = document.querySelector('.cardPayment');
 const invoicePayment = document.querySelector('.invoicePayment');
 
-const socialSecurityField = document.querySelector('#socialSecurity');
+const formOrderSocialSecurity = document.querySelector('#socialSecurity');
 
 const gdpr = document.querySelector('#gdpr');
 const newsletter =document.querySelector('#newsletter');
@@ -144,217 +128,193 @@ let isInvoice = false;
 let isSocialSecurity = false;
 let isGdpr = false;
   
+/************* ARRAY ********************************************/
 
+//  const formOrderInputs = Array.from(document.querySelector('.formOrder').querySelectorAll('input'));
 
- const formOrderInputs = Array.from(document.querySelector('.formOrder').querySelectorAll('input'));
+//  for (let i = 0; i < formOrderInputs.length; i++) {
+//   formOrderInputs[i].addEventListener('change', checkInputs);
+// }
+
+// console.log(formOrderInputs);
 
 /************* EVENT LISTENERS ********************************************/
- for (let i = 0; i < formOrderInputs.length; i++) {
-   formOrderInputs[i].addEventListener('change', checkInputs);
- }
 
-  // formOrderFirstName.addEventListener('change', checkName);
-  // formOrderLastName.addEventListener('change', checkLastname);
-  // formOrderAdress.addEventListener('change', checkAdress);
-  // formOrderZipcode.addEventListener('change', checkZip);
-  // formOrderCity.addEventListener('change', checkCity);
-  // formOrderPhone.addEventListener('change', checkPhone);
-  // formOrderEmail.addEventListener('change', checkEmail);
-  // socialSecurityField.addEventListener('change', checkSocialSecurity);
-
-console.log(formOrderInputs);
+  formOrderFirstName.addEventListener('change', checkName);
+  formOrderLastName.addEventListener('change', checkLastname);
+  formOrderAdress.addEventListener('change', checkAdress);
+  formOrderZipcode.addEventListener('change', checkZip);
+  formOrderCity.addEventListener('change', checkCity);
+  formOrderPhone.addEventListener('change', checkPhone);
+  formOrderEmail.addEventListener('change', checkEmail);
+  formOrderSocialSecurity.addEventListener('change', checkSocialSecurity);
 
 /************* FUNCTION CHECKNAME ********************************************/
 
- const firstnameRGEX = /^[a-zåäöü\-.\s]{2,}$/i;
- const lastnameRGEX = /^[a-zåäöü\-.\s]{2,}$/i;
+//  const firstnameRGEX = /^[a-zåäöü\-.\s]{2,}$/i;
+//  const lastnameRGEX = /^[a-zåäöü\-.\s]{2,}$/i;
 
-function checkInputs(e) { 
+// function checkInputs(e) { 
   // const input = e.currentTarget.id;
   // const value = e.currentTarget.value;
   // console.log(e.currentTarget.id);
   // console.log(e.currenTarget.value);
-   let getId = e.target.id;
-   let getValue = e.target.value;
-   console.log(e.target.id);
-   console.log(e.target.value);
+  //  let getId = e.target.id;
+  //  let getValue = e.target.value;
+  //  console.log(e.target.id);
+  //  console.log(e.target.value);
 
-  // if (getId == 'debitKredit' && e.target.checked) {
-  //   isDebitKredit = true;
-  //   isInvoice = false;
-  //   document.querySelector('#socialSecurity').required = false;
-  // }
-  // if (getId == 'invoice' && e.target.checked) {
-  //   isInvoice = true;
-  //   isDebitKredit = false;
-  //   document.querySelector('#socialSecurity').required = true;
-  // }
-  //   if (getId == 'gdpr' && e.target.checked) {
-  //     isGdpr = true;
-  //   } else if (getId == 'gdpr' && !e.target.checked) {
-  //     isGdpr = false;
-  //   }
-  //   console.log(e.target);
   // checkFormValid();
 
+//   if (firstnameRGEX.test(getId && getValue)) {
+//     isLastname = true;
+//     nameError.innerHTML = `<span class="material-symbols-outlined">
+//    check
+//    </span>`;
+//  } else {
+//   isFirstname = false;
+//    nameError.innerHTML = '  ange namn med minst 2 bokstäver.';
+//  }
 
-  if (firstnameRGEX.test(getId && getValue)) {
-    isLastname = true;
-    nameError.innerHTML = `<span class="material-symbols-outlined">
-   check
-   </span>`;
- } else {
-  isFirstname = false;
-   nameError.innerHTML = '  ange namn med minst 2 bokstäver.';
- }
-
- if( lastnameRGEX.test(getId && getValue)) {
-  isLastname = true;
-   lastnameError.innerHTML = `<span class="material-symbols-outlined">
-   check
-   </span>`
- } else {
-  isFirstname = false;
-   lastnameError.innerHTML = '  ange namn med minst 2 bokstäver.';
- }
- checkFormValid();
-};
-
-
-
+//  if( lastnameRGEX.test(getId && getValue)) {
+//   isLastname = true;
+//    lastnameError.innerHTML = `<span class="material-symbols-outlined">
+//    check
+//    </span>`
+//  } else {
+//   isFirstname = false;
+//    lastnameError.innerHTML = '  ange namn med minst 2 bokstäver.';
+//  }
+//  checkFormValid();
 // };
 
 
-// checkFormValid();
-function checkName() { 
-  if (/^[a-zåäöü\-.\s]{2,}$/i.test(formOrderFirstName.value)) { 
-    isFirstname = true;
-   nameError.innerHTML = `<span class="material-symbols-outlined">
-   check
-   </span>`;
- } else {
-  isFirstname = false;
-   nameError.innerHTML = '  ange namn med minst 2 bokstäver.';
- }
- checkFormValid();
+function checkInputs() {} 
 
-//  console.log ('namnet är', isFirstname);
-//  console.log(formOrderFirstName.value);
-}
-
-function checkLastname() { 
-  if (/^[a-zåäöü\-.\s]{2,}$/i.test(formOrderLastName.value)){
-    isLastname = true;
-    lastnameError.innerHTML = `<span class="material-symbols-outlined">
-   check
-   </span>`;
- } else {
-    isLastname = false;
-    lastnameError.innerHTML = ' ange efternamn med minst 2 bokstäver.';
- }
- checkFormValid();
-
-//  console.log ('efternamnet är', isLastname);
-//  console.log(formOrderLastName.value);
-}
-
-function checkAdress() {
-  if (/^(?:[A-ZÅÄÖ0-9]+\s)\d{1,}\s{0,7}(?:[A-ZÅÄÖ0-9#])|(?:[A-ZÅÄÖ]+\s){0,3}(?:[A-ZÅÄÖ]+)\s*\d{1,}$/i.test(formOrderAdress.value)) {
-    isAdress = true;
-    adressError.innerHTML = `<span class="material-symbols-outlined">
-  check
-  </span>`;
-} else {
-  isAdress = false;
-    adressError.innerHTML = ' ange gata och gatnummer.';
-}
-checkFormValid();
-}
-
-function checkZip() { 
-   if (/^[0-9]{3}\s?[0-9]{2}\s?$/.test(formOrderZipcode.value)) { 
-    isZipcode = true;
-    zipError.innerHTML = `<span class="material-symbols-outlined">
+  function checkName() { 
+    if (/^[a-zåäöü\-.\s]{2,}$/i.test(formOrderFirstName.value)) { 
+      isFirstname = true;
+      nameError.innerHTML = `<span class="material-symbols-outlined">
     check
     </span>`;
   } else {
-    isZipcode = false;
-    zipError.innerHTML = '  ange 5 siffror.';
-  }
+      isFirstname = false;
+      nameError.innerHTML = '  ange namn med minst 2 bokstäver.';
+    }
   checkFormValid();
-}
 
-function checkCity() {
-  if (/^[a-zåäöü\-.\s]{2,}$/i.test(formOrderCity.value)) {
-    isCity = true;
-    cityError.innerHTML = `<span class="material-symbols-outlined">
+   console.log ('namnet är', isFirstname);
+   console.log(formOrderFirstName.value);
+
+  }
+
+  function checkLastname() { 
+    if (/^[a-zåäöü\-.\s]{2,}$/i.test(formOrderLastName.value)){
+      isLastname = true;
+      lastnameError.innerHTML = `<span class="material-symbols-outlined">
     check
     </span>`;
   } else {
-    isCity = false;
-    cityError.innerHTML = ' ange postort med minst 2 bokstäver';
+      isLastname = false;
+      lastnameError.innerHTML = ' ange efternamn med minst 2 bokstäver.';
   }
   checkFormValid();
-  // console.log ('city är', isCity);
-  // console.log(formOrderCity.value);
-}
 
-function checkPhone() {
-  if (/^(\s*(7)|07)([0-9][ -]*){7}[0-9]$/.test(formOrderPhone.value)){
-    isTelephone = true;
-    phoneError.innerHTML = `<span class="material-symbols-outlined">
+    console.log ('efternamnet är', isLastname);
+    console.log(formOrderLastName.value);
+  }
+
+  function checkAdress() {
+    if (/^(?:[A-ZÅÄÖ0-9]+\s)\d{1,}\s{0,7}(?:[A-ZÅÄÖ0-9#])|(?:[A-ZÅÄÖ]+\s){0,3}(?:[A-ZÅÄÖ]+)\s*\d{1,}$/i.test(formOrderAdress.value)) {
+      isAdress = true;
+      adressError.innerHTML = `<span class="material-symbols-outlined">
     check
     </span>`;
   } else {
-    isTelephone = false;
-    phoneError.innerHTML = '  ange mobilnummer som börjar på 07 eller 7';
+    isAdress = false;
+      adressError.innerHTML = ' ange gata och gatnummer.';
   }
   checkFormValid();
-}
 
-function checkEmail() {
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i.test(formOrderEmail.value)){
-    isEmail = true;
-    emailError.innerHTML = `<span class="material-symbols-outlined">
-    check
-    </span>`;
-  } else {
-    isEmail = false;
-    emailError.innerHTML = '  ange giltig epostadress';
+    console.log ('adress is', isLastname);
+    console.log(formOrderAdress.value);
   }
-  checkFormValid();
-}
 
-// Open card payment option
-function cardPaymentOpen() {
-  if (cardRadio.checked) {
-    cardPayment.classList.add('paymentOpen');
-    invoicePayment.classList.remove('paymentOpen');
-  }
-  checkFormValid();
-}
+  function checkZip() { 
+    if (/^[0-9]{3}\s?[0-9]{2}\s?$/.test(formOrderZipcode.value)) { 
+      isZipcode = true;
+      zipError.innerHTML = `<span class="material-symbols-outlined">
+      check
+      </span>`;
+    } else {
+      isZipcode = false;
+      zipError.innerHTML = '  ange 5 siffror.';
+    }
+    checkFormValid();
 
-// Open invoice payment option
-function invoicePaymentOpen() {
-  if (invoiceRadio.checked) {
-    invoicePayment.classList.add('paymentOpen');
-    cardPayment.classList.remove('paymentOpen');
+    console.log ('zip är', isZipcode);
+    console.log(formOrderZipcode.value);
   }
-  checkFormValid();
-}
 
-function checkSocialSecurity() {
-  if(/^(19|20)?(\d{6}([-+]|\s)\d{4}|(?!19|20)\d{10})$/.test(socialSecurityField.value)) {
-    isSocialSecurity = true;
-    socialSecurityError.innerHTML = ` <span class="material-symbols-outlined">
-    check
-    </span>`;
-  } else {
-    isSocialSecurity = false;
-    socialSecurityError.innerHTML = ` ange giltigt personnummer, 10 eller 12 siffror`;
+  function checkCity() {
+    if (/^[a-zåäöü\-.\s]{2,}$/i.test(formOrderCity.value)) {
+      isCity = true;
+      cityError.innerHTML = `<span class="material-symbols-outlined">
+      check
+      </span>`;
+    } else {
+      isCity = false;
+      cityError.innerHTML = ' ange postort med minst 2 bokstäver';
+    }
+    checkFormValid();
+     console.log ('city är ', isCity);
+     console.log(formOrderCity.value);
   }
-  checkFormValid();
-}
+
+  function checkPhone() {
+    if (/^(\s*(7)|07)([0-9][ -]*){7}[0-9]$/.test(formOrderPhone.value)){
+      isTelephone = true;
+      phoneError.innerHTML = `<span class="material-symbols-outlined">
+      check
+      </span>`;
+    } else {
+      isTelephone = false;
+      phoneError.innerHTML = '  ange mobilnummer som börjar på 07 eller 7';
+    }
+    console.log ('phone is ', isCity);
+    console.log(formOrderPhone.value);
+    checkFormValid();
+  }
+
+  function checkEmail() {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i.test(formOrderEmail.value)){
+      isEmail = true;
+      emailError.innerHTML = `<span class="material-symbols-outlined">
+      check
+      </span>`;
+    } else {
+      isEmail = false;
+      emailError.innerHTML = '  ange giltig epostadress';
+    }
+    console.log ('email is ', isCity);
+    console.log(formOrderEmail.value);
+    checkFormValid();
+  }
+
+  function checkSocialSecurity() {
+    if(/^(19|20)?(\d{6}([-+]|\s)\d{4}|(?!19|20)\d{10})$/.test(formOrderSocialSecurity.value)) {
+      isSocialSecurity = true;
+      socialSecurityError.innerHTML = ` <span class="material-symbols-outlined">
+      check
+      </span>`;
+    } else {
+      isSocialSecurity = false;
+      socialSecurityError.innerHTML = ` ange giltigt personnummer, 10 eller 12 siffror`;
+    }
+    console.log ('id is ', isSocialSecurity);
+    console.log(formOrderSocialSecurity.value);
+    checkFormValid();
+  }
 
 
 
@@ -415,8 +375,8 @@ function checkSocialSecurity() {
       isCity &&
       isTelephone &&
       isEmail &&
-      isDebitCredit //|| (isInvoice && isSocialSecurity)) &&
-      // isGdpr
+      //isDebitCredit //|| (isInvoice && isSocialSecurity)) &&
+      isGdpr
       )
       {
 
